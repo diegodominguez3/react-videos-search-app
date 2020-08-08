447fdb1db0f49fc07afb048340765d732cbf70c0
Diego Dominguez <diegodominguez333@gmail.com>
2020-08-08 15:42:03 -0600

    Updates

---
+++ precache-manifest.2bb2652412db9bef98a45a0b02e9eb71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2c29ed0db3fa1bedd7de0102b462bd7",
+    "url": "/react-videos-search-app/index.html"
+  },
+  {
+    "revision": "da521a15d752f48c91a9",
+    "url": "/react-videos-search-app/static/css/main.bbe57b6a.chunk.css"
+  },
+  {
+    "revision": "17bd7c613a068a1f8918",
+    "url": "/react-videos-search-app/static/js/2.35399e9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-videos-search-app/static/js/2.35399e9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da521a15d752f48c91a9",
+    "url": "/react-videos-search-app/static/js/main.bb62d042.chunk.js"
+  },
+  {
+    "revision": "4d6b9464c51f56c58975",
+    "url": "/react-videos-search-app/static/js/runtime-main.2b22eb5d.js"
+  }
+]);
